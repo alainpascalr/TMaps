17b726e5b2a5d09087459372608b71f13338dc2f
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2017-01-21 15:18:29 -0500

    Sme Receive implemented

--- app/src/main/java/ca/messenger/MainActivity.java
+++ app/src/main/java/ca/messenger/MainActivity.java
@@ -1,13 +1,46 @@
 package ca.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+//    Button btnSendSMS;
+    IntentFilter intentFilter;
+    private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //---display the SMS received in the TextView---
+            TextView SMSes = (TextView) findViewById(R.id.textView1);
+            SMSes.setText(intent.getExtras().getString("sms"));
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
     }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(intentReceiver, intentFilter);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        unregisterReceiver(intentReceiver);
+        super.onPause();
+    }
+
+
 }
+
