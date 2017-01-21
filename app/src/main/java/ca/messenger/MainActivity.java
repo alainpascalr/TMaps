91c57095020dde65fd1d9844d1628f3b8bac29da
Hamid Habib <hamidhabib@hamidhabib.wireless.concordia.ca>
2017-01-21 13:45:48 -0500

    internet connection working

--- app/src/main/java/ca/messenger/MainActivity.java
+++ app/src/main/java/ca/messenger/MainActivity.java
@@ -1,27 +1,52 @@
 package ca.messenger;
 
-import android.os.StrictMode;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    CheckInternetConnection checkInternetConnection = new CheckInternetConnection();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Hack for bypassing the Async Strict mode
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-        if(checkInternetConnection.isInternetWorking()){
-            Log.d("SUCCESS", "PRETTY GOOD");
-        } else {
-            Log.d("FAILURE", "PRETTY NOT GRESAT");
-
-        }
+        final Button Save = (Button) findViewById(R.id.save);
+        Save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isInternetOn();
+            }
+        });
     }
 
+
+    public final boolean isInternetOn() {
+
+        // get Connectivity Manager object to check connection
+        ConnectivityManager connec =
+                (ConnectivityManager)getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+
+        // Check for network connections
+        if ( connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ) {
+
+            // if connected with internet
+
+            Toast.makeText(this, " Connected ", Toast.LENGTH_LONG).show();
+            return true;
+
+        } else if (
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                        connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
+
+            Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return false;
+    }
 }
