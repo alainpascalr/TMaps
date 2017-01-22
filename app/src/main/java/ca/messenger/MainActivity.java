f3d3b1bc883d31ed42f65533f9157c5bdac0e0f2
Hamid Habib <hamidhabib@hamidhabib.wireless.concordia.ca>
2017-01-22 05:42:38 -0500

    yellowpages api added

--- app/src/main/java/ca/messenger/MainActivity.java
+++ app/src/main/java/ca/messenger/MainActivity.java
@@ -3,24 +3,34 @@
  */
 package ca.messenger;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
-import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    //Yellow Pages
+    ProgressDialog progressDialog;
+    Button getYellowPagesData;
+    TextView yellowPagesResult;
 
     IntentFilter intentFilter;
     private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
@@ -42,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         //Get Directions
         NewDirections newDirections = new NewDirections();
         try {
-
             steplist = newDirections.getNewDirections();
             for(int i = 0; i < steplist.size(); i++){
                 System.out.println(steplist.get(i));
@@ -54,8 +63,19 @@ public class MainActivity extends AppCompatActivity {
         isInternetOn();
         intentFilter = new IntentFilter();
         intentFilter.addAction("SMS_RECEIVED_ACTION");
-    }
 
+        //Get Yellow pages data
+        getYellowPagesData = (Button) findViewById(R.id.btnHit);
+        yellowPagesResult = (TextView) findViewById(R.id.tvJsonItem);
+
+        getYellowPagesData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute("http://api.sandbox.yellowapi.com/FindBusiness/?what=Restaurants&where=Toronto&pgLen=5&pg=1&dist=1&fmt=JSON&lang=en&UID=USER001&apikey=c8bk7t7ay794pynf7xxaapnh\n");
+            }
+        });
+    }
+    
     @Override
     protected void onResume() {
         registerReceiver(intentReceiver, intentFilter);
@@ -87,5 +107,58 @@ public final boolean isInternetOn() {
     }
     return false;
 }
+
+    // Yellow pages API
+    private class JsonTask extends AsyncTask<String, String, String> {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+                }
+                return buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+            yellowPagesResult.setText(result);
+        }
+    }
 }
 
---
+++ app/src/main/java/ca/messenger/yellowpages.java
@@ -0,0 +1,8 @@
+package ca.messenger;
+
+/**
+ * Created by hamidhabib on 2017-01-22.
+ */
+
+public class yellowpages {
+}
